67e124ae49dce0606e5f4d62a9209b5ebeb77607
rensygifani <135991385+rensygifani@users.noreply.github.com>
2023-12-20 15:45:51 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app" class="container">
+    <h1 class="text-center">Draw a Digit</h1>
+    <DrawingBoard />
+  </div>
+</template>
+
+<script>
+import DrawingBoard from './components/DrawingBoard.vue';
+
+export default {
+  name: 'App',
+  components: {
+    DrawingBoard,
+  },
+};
+</script>
+
+<style scoped>
+/* Scoped styles for App.vue if needed */
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/styles.css';  // Import custom styles
+import App from './App.vue';
+
+createApp(App).mount('#app');
